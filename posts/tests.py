e0fd65535aba31d4d5ebddcd3d12843b4dc82aae
dinesh <dineshpy07@gmail.com>
2018-11-22 18:00:06 +0530

    Add a simple test

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,30 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
 from posts.models import Post, Comment
 
 
+class PostTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        post = Post.objects.create(
+            title="post1",
+            description="post1 description"
+        )
+
+        Comment.objects.create(
+            text="Comment for post1",
+            post=post
+        )
+
+    def test_get_posts(self):
+        response = self.client.get("/api/posts/", format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_comments(self):
+        response = self.client.get("/api/comments/", format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
